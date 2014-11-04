7f3ba181d18716b765538133a6d263747fd56e4a
Zachary Perlmutter <zrp200@gmail.com>
2014-11-03 18:04:13 -0800

    Update card.rb
    [skip ci]

--- lib/card.rb
+++ lib/card.rb
@@ -3,7 +3,7 @@ class CardDeck::Card
 	def value # The points a card is worth
 		case num
 			when 2..10 then num.to_i
-			when "Ace" then [1, 11]
+			when "Ace" then 11
 			when "Jack" then 10
 			when "Queen" then 10
 			when "King" then 10
