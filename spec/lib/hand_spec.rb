03ce1d11a016c30a5f9dee6676172237646475c1
Zachary Perlmutter <zrp200@gmail.com>
2014-11-06 17:26:09 -0800

    Update hand_spec.rb
    [skip ci]

--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -10,17 +10,6 @@ RSpec.describe Hand do
     it {is_expected.to be_an_instance_of Array}
   end
   describe "#new" do
-    describe "blackjack?" do
-      s, s.cards = Hand.new, CardDeck.Card("Ace"), CardDeck.Card("Jack")
-      subject {s}
-      it {is_expected.to have_blackjack}
-    end
-    describe "bust?" do
-      s = Hand.new
-      s.cards = [CardDeck.Card(10), CardDeck.Card("King"), CardDeck.Card("Jack")]
-      subject {s}
-      it {is_expected.to bust}
-    end
     describe "hit" do
       s = Hand.new
       subject {s.cards}
