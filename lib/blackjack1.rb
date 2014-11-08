49ade93fea689688ca1190c9f6967b8e0728951e
Zachary Perlmutter <zrp200@gmail.com>
2014-11-08 10:43:22 -0800

    Update blackjack1.rb

--- lib/blackjack1.rb
+++ lib/blackjack1.rb
@@ -1,2 +1,16 @@
 require_relative "hand.rb'
-def scoring
+def compare_score(p1, p2, args=Hash.new(false)) # Determines who wins. parameter p1 is the player and parameter p2 is the dealer.
+  unless args[:compare]
+    win, lose, tie = "You win!", "Dealer wins.", "Tie"
+  else
+    win, lose, tie = true, false, nil
+  end
+  if p1.bust? and p2.bust? then return tie
+  elsif p2.bust? then return win
+  elsif p1.bust? then return lose
+  elsif p1.value > p2.value then return win
+  elsif p2.value > p1.value then return lose
+  else
+    return tie
+  end
+end
